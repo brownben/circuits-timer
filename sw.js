9bbed6c532ec1694e9d67e1b7eb253a79342c136
brownben <9870007+bonalybob@users.noreply.github.com>
2018-09-03 18:34:13 +0100

    Feature - PWA
    Add a service worker so it works offline as a Progressive Web App

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('circuits').then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/logo.png',
+                '/index.js',
+                '/style.css',
+                '/style.css.map',
+            ])
+        })
+    )
+})
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request)
+        })
+    )
+})
