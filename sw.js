c0499c00c06f8ae68acbca42e9b6746dc04fd2ff
bonalybob <bonalybob@users.noreply.github.com>
2018-10-13 21:04:38 +0100

    Change Cache
    Save offline to a different cache so not confused with old version

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function (e) {
     e.waitUntil(
-        caches.open('circuits').then(function (cache) {
+        caches.open('circuits-1').then(function (cache) {
             return cache.addAll([
                 './',
                 './index.html',
