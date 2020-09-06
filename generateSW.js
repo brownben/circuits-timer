bac2fadd1bb23c129758e8813d7b52bf866af30c
brownben <9870007+brownben@users.noreply.github.com>
2020-09-06 19:47:46 +0100

    Feature - Add Service Worker and Icons

---
+++ generateSW.js
@@ -0,0 +1,14 @@
+const { generateSW } = require('rollup-plugin-workbox')
+
+generateSW({
+  swDest: '../docs/service-worker.js',
+  globDirectory: './docs',
+  globPatterns: ['**/*.{html,json,js,css,woff2,png,svg,txt}'],
+
+  cacheId: 'circuits-timer-precache',
+  sourcemap: false,
+  inlineWorkboxRuntime: true,
+  mode: 'production',
+
+  navigateFallback: '/circuits-timer/index.html',
+}).writeBundle()
